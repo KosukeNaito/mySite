67e5950934e0b6224def07600a818bab5da82902
Kosuke Naito <tonightoo7110@gmail.com>
2019-03-04 03:10:03 +0900

    test

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,15 +4,13 @@
 # --- !Ups
 
 create table account (
-  name                     varchar(255) not null,
+  name                          varchar(255) not null,
   password                      varchar(255) not null,
-  constraint uq_account_name unique (name),
   constraint pk_account primary key (name)
 );
-create sequence account_seq;
 
 
 # --- !Downs
 
 drop table if exists account;
-drop sequence if exists account_seq;
+
